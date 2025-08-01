fd10b72d06b5d34a918bdff644657c39563d4a7a
huijuhnkim <136677687+huijuhnkim@users.noreply.github.com>
2025-07-31 21:35:54 -0400

    Initial Commit

---
+++ EPPATests/EPPATests.swift
@@ -0,0 +1,17 @@
+//
+//  EPPATests.swift
+//  EPPATests
+//
+//  Created by Hugh Kim on 7/31/25.
+//
+
+import Testing
+@testable import EPPA
+
+struct EPPATests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
